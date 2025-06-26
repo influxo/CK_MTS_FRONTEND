2d93e7f43423c530ef54eb69c209f481c29fea13
L1smanity <lismanity@gmail.com>
2025-06-26 22:54:51 +0200

    small change

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,18 +1,21 @@
-import { FilterControls } from "../components/FilterControls";
-import { SummaryMetrics } from "../components/SummaryMetrics";
-import { FormSubmissions } from "../components/FormSubmissions";
-import { KpiHighlights } from "../components/KpiHighlights";
-import { SyncStatus } from "../components/SyncStatus";
-import { SystemAlerts } from "../components/SystemAlerts";
-import { BeneficiaryDemographics } from "../components/BeneficiaryDemographics";
-import { ServiceDelivery } from "../components/ServiceDelivery";
-import { RecentActivity } from "../components/RecentActivity";
+import { FilterControls } from "../components/dashboard/FilterControls";
+import { SummaryMetrics } from "../components/dashboard/SummaryMetrics";
+import { FormSubmissions } from "../components/dashboard/FormSubmissions";
+import { KpiHighlights } from "../components/dashboard/KpiHighlights";
+import { SyncStatus } from "../components/dashboard/SyncStatus";
+import { SystemAlerts } from "../components/dashboard/SystemAlerts";
+import { BeneficiaryDemographics } from "../components/dashboard/BeneficiaryDemographics";
+import { ServiceDelivery } from "../components/dashboard/ServiceDelivery";
+import { RecentActivity } from "../components/dashboard/RecentActivity";
 export function Dashboard() {
+  // TODO: ka ndryshim t components me /dashboard edhe pa to, ama e ngjashme
+
   return (
     <>
       <FilterControls />
       <SummaryMetrics />
 
+      {/* FormSubmissions prej /components/FormSubmissions o ndryshe prej ksaj, ama e ngjashme */}
       <FormSubmissions />
 
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 mb-6">
